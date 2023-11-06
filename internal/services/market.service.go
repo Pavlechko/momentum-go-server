28691cbf25241a8c02ad5c01ebb65dae185c90ad
Pavlechko <pavlechko.serhii@gmail.com>
2023-11-06 21:49:06 +0100

    Added error handling and writing its to the log file in Market service

--- internal/services/market.service.go
+++ internal/services/market.service.go
@@ -3,8 +3,8 @@ package services
 import (
 	"encoding/json"
 	"io"
-	"log"
 	"momentum-go-server/internal/models"
+	"momentum-go-server/internal/utils"
 	"net/http"
 	"os"
 
@@ -13,22 +13,25 @@ import (
 
 func GetMarketData() models.StockMarketResponse {
 	var response models.StockMarket
+	var frontendResponse models.StockMarketResponse
 
 	resp, err := http.Get("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=DAX&apikey=" + os.Getenv("STOCK_MARKET_API"))
 
 	if err != nil {
-		log.Println("Error creating HTTP request:", err)
+		utils.ErrorLogger.Println("Error creating HTTP request:", err)
+		return frontendResponse
 	}
 
 	defer resp.Body.Close()
 	body, err := io.ReadAll(resp.Body)
 
 	if err != nil {
-		log.Println("Error reading HTTP response body:", err)
+		utils.ErrorLogger.Println("Error reading HTTP response body:", err)
+		return frontendResponse
 	}
 
 	json.Unmarshal([]byte(body), &response)
-	frontendResponse := models.StockMarketResponse{
+	frontendResponse = models.StockMarketResponse{
 		Symbol:        response.Market.Symbol,
 		Price:         response.Market.Price,
 		Change:        response.Market.Change,

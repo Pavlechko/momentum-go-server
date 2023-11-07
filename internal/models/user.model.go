31986b998c87ac0e147cf0c607ae4c4476e1bf64
Pavlechko <pavlechko.serhii@gmail.com>
2023-11-07 17:15:49 +0100

    Implemented the creation and automatic migration of the Settings table, as well as added methods for creating and receiving them

--- internal/models/user.model.go
+++ internal/models/user.model.go
@@ -7,10 +7,10 @@ import (
 )
 
 type User struct {
-	ID uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
-	// ID     uint   `gorm:"primary_key"`
-	Hashpassword string `gorm:"type:varchar(255);not null"`
-	Name         string `gorm:"uniqueIndex;not null"`
+	ID           uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primary_key"`
+	Hashpassword string    `gorm:"type:varchar(255);not null"`
+	Name         string    `gorm:"uniqueIndex;not null"`
+	Settings     []Setting
 	CreatedAt    time.Time
 	UpdatedAt    time.Time
 }

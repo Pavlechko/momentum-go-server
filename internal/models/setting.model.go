f3294649c87caefa622b773a95a0ab6fec027372
Pavlechko <pavlechko.serhii@gmail.com>
2023-11-08 17:01:14 +0100

    fix: Implemented the ValueMap type with methods for interpreting []byte from jsonb and demarshaling it to the ValueMap type

--- internal/models/setting.model.go
+++ internal/models/setting.model.go
@@ -1,16 +1,36 @@
 package models
 
 import (
+	"database/sql/driver"
+	"encoding/json"
+	"fmt"
 	"time"
 
 	"github.com/google/uuid"
 )
 
+type ValueMap map[string]string
+
 type Setting struct {
-	ID        int               `gorm:"type:int;primary_key"`
-	UserID    uuid.UUID         `gorm:"type:uuid;not null"`
-	Name      string            `gorm:"type:varchar(255);not null"`
-	Value     map[string]string `gorm:"type:jsonb;not null"`
+	ID        int       `gorm:"type:int;primary_key"`
+	UserID    uuid.UUID `gorm:"type:uuid;not null"`
+	Name      string    `gorm:"type:varchar(255);not null"`
+	Value     ValueMap  `gorm:"type:jsonb;not null"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
+}
+
+func (v ValueMap) Value() (driver.Value, error) {
+	return json.Marshal(v)
+}
+
+func (v *ValueMap) Scan(src interface{}) error {
+	switch src := src.(type) {
+	case []byte:
+		return json.Unmarshal(src, v)
+	case string:
+		return json.Unmarshal([]byte(src), v)
+	default:
+		return fmt.Errorf("unsupported type: %T", src)
+	}
 }

11af044ded8f7af9ebdd942c9b503fb8b0ee9e3e
Pavlechko <pavlechko.serhii@gmail.com>
2023-11-09 14:41:21 +0100

    refactor: [MOM-54] Creating function that returns gefault settings

--- internal/utils/token.go
+++ internal/utils/token.go
@@ -3,7 +3,9 @@ package utils
 import (
 	"fmt"
 	"momentum-go-server/internal/models"
+	"net/http"
 	"os"
+	"strings"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -30,4 +32,36 @@ func GenerateJWT(user *models.UserResponse) (string, error) {
 	}
 
 	return tokenString, err
+}
+
+func GetUserId(r *http.Request) string {
+	if r.Header["Authorization"] != nil {
+
+		authHeader := strings.Split(r.Header.Get("Authorization"), "Bearer ")
+
+		if len(authHeader) != 2 {
+			ErrorLogger.Println("Malformed Token")
+			return ""
+		} else {
+			jwtToken := authHeader[1]
+			parsedToken, _ := jwt.Parse(jwtToken, func(t *jwt.Token) (interface{}, error) {
+				return []byte(os.Getenv("API_SECRET")), nil
+			})
+
+			claims, ok := parsedToken.Claims.(jwt.MapClaims)
+			if ok {
+				id, ok := claims["userId"].(string)
+				if !ok {
+					ErrorLogger.Println("Error parsing userId")
+				}
+				return id
+			} else {
+				ErrorLogger.Println("Error parsing token")
+				return ""
+			}
+		}
+	} else {
+		ErrorLogger.Println("You're Unauthorized due to No token in the header")
+		return ""
+	}
 }

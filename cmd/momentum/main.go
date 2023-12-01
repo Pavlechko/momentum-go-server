e72cde193e0f7caef1d5a4be99edc537932e2f72
Pavlechko <pavlechko.serhii@gmail.com>
2023-12-01 08:55:30 +0100

    refactor:[MOM-70] Rewrote Router and initialization of service and handler in Main

--- cmd/momentum/main.go
+++ cmd/momentum/main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"momentum-go-server/internal/handlers"
 	"momentum-go-server/internal/routes"
+	"momentum-go-server/internal/services"
 	"momentum-go-server/internal/store"
+	"momentum-go-server/internal/utils"
 )
 
 func main() {
-	store.ContentDB()
-	server := routes.NewAPIServer(":8080")
+	db, err := store.ConnectDB()
+	if err != nil {
+		utils.ErrorLogger.Println("Error connectiont to database:", err)
+	}
+	defer db.Close()
+
+	service := services.CreateService(db)
+	handler := handlers.CreateHandler(service)
+
+	server := routes.NewAPIServer(":8080", handler)
 	server.Run()
 }

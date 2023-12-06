f2632b3e06b2a9fb8a85fa1455f0ba328be408c8
Pavlechko <pavlechko.serhii@gmail.com>
2023-12-06 08:29:06 +0100

    refactor: Moved user input validation from the service to the handler

--- internal/handlers/user.handler.go
+++ internal/handlers/user.handler.go
@@ -1,17 +1,30 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
 	"momentum-go-server/internal/models"
 	"momentum-go-server/internal/utils"
 )
 
+const minNameLeng = 3
+const minPassLeng = 6
+
 func (h *Handler) SignUpHandler(w http.ResponseWriter, r *http.Request) {
 	var user models.UserInput
 
 	if !h.IsDecodeJSONRequest(w, r, &user) {
 		return
+	}
+
+	err := validateUser(user)
+
+	if err != nil {
+		err = h.WriteJSONError(w, http.StatusConflict, err.Error())
+		if err != nil {
+			utils.ErrorLogger.Printf("Error write errorJSON %s", err.Error())
+		}
 	}
 
 	token, err := h.Service.CreateUser(user)
@@ -33,6 +46,15 @@ func (h *Handler) SignInHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	err := validateUser(user)
+
+	if err != nil {
+		err = h.WriteJSONError(w, http.StatusConflict, err.Error())
+		if err != nil {
+			utils.ErrorLogger.Printf("Error write errorJSON %s", err.Error())
+		}
+	}
+
 	token, err := h.Service.GetUser(user)
 
 	if err != nil {
@@ -43,4 +65,14 @@ func (h *Handler) SignInHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	h.WriteToken(w, http.StatusOK, token)
+}
+
+func validateUser(user models.UserInput) error {
+	if len(user.Name) < minNameLeng {
+		return fmt.Errorf("your name is too short, min 3 symbols")
+	} else if len(user.Password) < minPassLeng {
+		return fmt.Errorf("your password is too short, min 6 symbols")
+	}
+
+	return nil
 }
